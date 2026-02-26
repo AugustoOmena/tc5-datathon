a9b5e3711a8827323b564ad81bce52045e9ba05d
Augusto Omena <122471298+AugustoOmena@users.noreply.github.com>
2026-02-26 14:46:02 -0300

    Add Terraform configuration for ECR repository and S3 bucket structure

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "envdev"
+}
+
+# Bucket S3 para armazenar tudo: Dados brutos, Parquets do Feast e Modelos
+resource "aws_s3_bucket" "mlops_artifacts" {
+  bucket = "tc5-mlops-artifacts-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+# Criando a estrutura de pastas dentro do S3
+resource "aws_s3_object" "folder_data" {
+  bucket = aws_s3_bucket.mlops_artifacts.id
+  key    = "data/"
+}
+
+resource "aws_s3_object" "folder_models" {
+  bucket = aws_s3_bucket.mlops_artifacts.id
+  key    = "models/"
+}
+
+# Saída do nome do bucket para usarmos depois
+output "s3_bucket_name" {
+  value = aws_s3_bucket.mlops_artifacts.bucket
+}
